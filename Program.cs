624c2a6d00dca9b8c76d16348690a8e5104c29ba
Tkacheva Diana <dianatka4eva@outlook.com>
2019-10-04 21:00:40 +0300

    lab1

--- Program.cs
+++ Program.cs
@@ -35,6 +35,47 @@ namespace Lab_1_sem3
                     val_c = getDbl("Введите коэффициент C: ");
             }
 
+
+            if (val_a == 0)
+            {
+                Console.WriteLine($"{val_b}x^2 + {val_c}=0");
+                if (-val_c / val_b < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Корней нет\n");
+                    Console.ResetColor();
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"x1 = {Math.Sqrt(-val_c / val_b)}; x2 = {-Math.Sqrt(-val_c / val_b)}");
+                    Console.ResetColor();
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            if (val_b == 0)
+            {
+                Console.WriteLine($"{val_a}x^4 + {val_c}=0");
+                if (-val_c / val_a < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Корней нет\n");
+                    Console.ResetColor();
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"x1 = {Math.Pow((-val_c / val_a), 1.0 / 4)}; x2 = {-Math.Pow((-val_c / val_a), 1.0 / 4)}");
+                    Console.ResetColor();
+                    Console.ReadLine();
+                    return;
+                }
+            }
             Console.WriteLine($"{val_a}x^4 + {val_b}x^2 + {val_c}=0");
             Console.WriteLine("Cделаем замену x^2=t и решим квадратное уравнение:");
             Console.WriteLine($"{val_a}t^2 + {val_b}t + {val_c}=0");
@@ -46,7 +87,7 @@ namespace Lab_1_sem3
             {
                 r1 = (-val_b + Math.Sqrt(descr)) / (2 * val_a);
                 r2 = (-val_b - Math.Sqrt(descr)) / (2 * val_a);
-                
+
             }
             else if (descr == 0)
             {
@@ -63,10 +104,13 @@ namespace Lab_1_sem3
 
             Console.WriteLine("Корни квадратного уравнения:");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"t1: {r1}; t2: {r2}");
+            if (r1 == r2)
+                Console.WriteLine($"t: {r1}");
+            else
+                Console.WriteLine($"t1: {r1}; t2: {r2}");
             Console.ResetColor();
             Console.WriteLine("Корни биквадратного уравнения:");
-            if (r1 < 0 || r2 < 0)
+            if (r1 < 0 && r2 < 0)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Корней нет\n");
@@ -75,57 +119,64 @@ namespace Lab_1_sem3
                 return;
             }
             Console.ForegroundColor = ConsoleColor.Green;
-            if (r1 != r2)
+
+            if (r1 == 0 && r2 == 0)
             {
-                bool a = false;
-                if (r1 == 0)
-                {
-                    Console.WriteLine($"x1 = {Math.Sqrt(r1)}");
-                    a = true;
-                }
+                Console.WriteLine("x = 0\n");
+                Console.ResetColor();
+                Console.ReadLine();
+                return;
+            }
+
+            if (r1 == r2)
+            {
+                Console.WriteLine($"x1 = {Math.Sqrt(r1)}; x2 = {-Math.Sqrt(r1)}");
+                Console.ReadLine();
+                return;
+            }
+
+            if (r1 == 0)
+            {
+                if (r2 < 0)
+                    Console.WriteLine($"x = 0");
                 else
-                    Console.WriteLine($"x1 = {Math.Sqrt(r1)}; x2 = {-Math.Sqrt(r1)}");
+                    Console.WriteLine($"x1 = 0; x2 = {Math.Sqrt(r2)}; x3 = {-Math.Sqrt(r2)}");
+            }
+            else if (r1 < 0)
+            {
                 if (r2 == 0)
-                {
-                    if (a)
-                        Console.WriteLine($"x2 = {Math.Sqrt(r1)}");
-                    else
-                        Console.WriteLine($"x3 = {Math.Sqrt(r1)}");
-                }
+                    Console.WriteLine($"x = 0");
                 else
-                {
-                    if(a)
-                        Console.WriteLine($"x2 = {Math.Sqrt(r2)}; x3 = {-Math.Sqrt(r2)}");
-                    else
-                        Console.WriteLine($"x3 = {Math.Sqrt(r2)}; x4 = {-Math.Sqrt(r2)}");
-                }
+                    Console.WriteLine($"x1 = {Math.Sqrt(r2)}; x2 = {-Math.Sqrt(r2)}");
             }
             else
             {
-                if (r1 == 0)
-                    Console.WriteLine("x1 = 0", ConsoleColor.Green);
+                if (r2 < 0)
+                    Console.WriteLine($"x1 = {Math.Sqrt(r1)}; x2 = {-Math.Sqrt(r1)}");
+                else if (r2 == 0)
+                    Console.WriteLine($"x1 = {Math.Sqrt(r1)}; x2 = {-Math.Sqrt(r1)}; x3 = 0");
                 else
-                    Console.WriteLine($"x1 = {Math.Sqrt(r1)}; x2 = {-Math.Sqrt(r1)}", ConsoleColor.Green);
+                    Console.WriteLine($"x1 = {Math.Sqrt(r1)}; x2 = {-Math.Sqrt(r1)}; x3 = {Math.Sqrt(r2)}; x4 = {-Math.Sqrt(r2)}");
             }
             Console.ResetColor();
             Console.ReadLine();
             Console.OutputEncoding = src;
-        }
-        static double getDbl(string message)
-        {
-            string resStr;
-            double resDbl;
-            bool correct;
-            do
+            double getDbl(string message)
             {
-                Console.Write(message);
-                resStr = Console.ReadLine();
-                correct = double.TryParse(resStr, out resDbl);
-                //if (!correct)
-                //    Console.WriteLine("Введите вещественное число!");
+                string resStr;
+                double resDbl;
+                bool correct;
+                do
+                {
+                    Console.Write(message);
+                    resStr = Console.ReadLine();
+                    correct = double.TryParse(resStr, out resDbl);
+                    //if (!correct)
+                    //    Console.WriteLine("Введите вещественное число!");
+                }
+                while (!correct);
+                return resDbl;
             }
-            while (!correct);
-            return resDbl;
         }
     }
 }
