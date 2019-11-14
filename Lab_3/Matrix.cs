84e8a1133d1186dac4f90ecbb459a1f9f292e929
Tkacheva Diana <dianatka4eva@outlook.com>
2019-11-14 23:25:08 +0300

    Lab 3

---
+++ Lab_3/Matrix.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_3_sem3
+{
+    public class Matrix<T>
+    {
+        private Dictionary<string, T> _matrix = new Dictionary<string, T>();
+        private int _maxX;
+        private int _maxY;
+        private int _maxZ;
+        private T _nullElement;
+
+        public Matrix(int px, int py, int pz, T nullElementParam)
+        {
+            _maxX = px;
+            _maxY = py;
+            _maxZ = pz;
+            _nullElement = nullElementParam;
+        }
+
+        public T this[int x, int y, int z]
+        {
+            get
+            {
+                CheckBounds(x, y, z);
+                string key = DictKey(x, y, z);
+                if (_matrix.ContainsKey(key))
+                {
+                    return _matrix[key];
+                }
+                else
+                {
+                    return _nullElement;
+                }
+            }
+            set
+            {
+                CheckBounds(x, y, z);
+                string key = DictKey(x, y, z);
+                _matrix.Add(key, value);
+            }
+        }
+
+        void CheckBounds(int x, int y, int z)
+        {
+            if (x < 0 || x >= _maxX) 
+                throw new Exception("x=" + x + " выходит за границы");
+            if (y < 0 || y >= _maxY) 
+                throw new Exception("y=" + y + " выходит за границы");
+            if (z < 0 || z >= _maxZ)
+                throw new Exception("z=" + z + " выходит за границы");
+        }
+
+        string DictKey(int x, int y, int z)
+        {
+            return x.ToString() + "_" + y.ToString() + "_" + z.ToString();
+        }
+
+        public override string ToString()
+        {
+            var b = new StringBuilder();
+            for (int j = 0; j < _maxY; j++)
+            {
+                b.Append("[");
+                for (int i = 0; i < _maxX; i++)
+                {
+                    for (int k = 0; k < _maxZ; k++)
+                    {
+                        var item = this[i, j, k];
+                        if (item != null)
+                            b.Append($"x:{j};y:{i};z:{k}, item: {item.ToString()}");
+                    }
+                }
+                b.Append("]\n");
+            }
+            return b.ToString();
+        }
+    }
+}
---
+++ Lab_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using Lab_2_sem3;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab_3_sem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList();
+            Rectangle rct = new Rectangle(20, 3);
+            Square sq = new Square(2);
+            Circle cr = new Circle(5);
+            arr.Add(rct);
+            arr.Add(sq);
+            arr.Add(cr);
+            foreach(var item in arr)
+            {
+                Console.WriteLine(((GeometricShape)item).CalculateShapeArea());
+            }
+            List<GeometricShape> sorted = new List<GeometricShape>();
+            sorted.Add(rct);
+            sorted.Add(sq);
+            sorted.Add(cr);
+            sorted.Sort();
+            foreach(var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nМатрица");
+            Matrix<GeometricShape> cube = new Matrix<GeometricShape>(3, 3, 3, null);
+            cube[0, 0, 0] = rct;
+            cube[1, 1, 1] = sq;
+            cube[2, 2, 2] = cr;
+            Console.WriteLine(cube.ToString());
+            Console.ReadLine();
+        }
+    }
+}
